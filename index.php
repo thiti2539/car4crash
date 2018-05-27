fc3636bc404f5decd7eeb4a502e7105e9ec2de64
DoopDip <narit.su@icloud.com>
2018-05-27 19:19:02 +0700

    xxx

--- index.php
+++ index.php
@@ -33,9 +33,10 @@
     <script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-firestore.js"></script>
     <script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-messaging.js"></script>
 
-    <!-- <script type="text/javascript" src="./js/bootstrap-material-datetimepicker.js"></script> -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>                       
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
 
     <style>
         .c-pointer {
